8c4c7d039472ecbe7a06d6711a01fc94fa76a579
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2020-06-13 16:27:29 +0300

    0

--- nav/nav.js
+++ nav/nav.js
@@ -1,16 +1,33 @@
-var style =  document.getElementById(`item`).style
 
-function openNav() {
-  if (style.display=="flex"){
-    return closeNav()
+// code new
+function view(id , value ){
+  var app =  document.getElementById(id)
+  if (app.style.display==value){
+    app.style.display = "none"; 
   }else{
-    style.display = "flex";
-    style.width = "100%";
+    app.style.display = value; 
   } 
+} 
+function app  (){ 
+    this.name="1";
+    this.an= "2";
+    this.adding= "3";
+    this.tools= "4" ;
+
+    this.course= { 
+        name_course: "5" ,
+        in_course: "6" ,
+        link_course: "7"
+    }
+    this.getin =gits
+    
 }
-
-function closeNav() { 
-    style.display = "none";
-    style.width = "0%"; 
-}  
-
+function gits(){ 
+  console.log(
+   this.name+"-"+
+         this.an+"-"+
+         this.adding+"-"+
+         this.tools+"-"+
+         this.course
+  )
+}
