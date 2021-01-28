e5387c153cbcab96eb84c9cf78b31f9ca5882a48
ktsyr1 <ktsyr1@gmail.com>
2021-01-28 12:09:31 +0200

    Update nav.js

--- nav/nav.js
+++ nav/nav.js
@@ -1,33 +1,33 @@
 
 // code new
-function view(id , value ){
-  var app =  document.getElementById(id)
-  if (app.style.display==value){
-    app.style.display = "none"; 
-  }else{
-    app.style.display = value; 
-  } 
-} 
-function app  (){ 
-    this.name="1";
-    this.an= "2";
-    this.adding= "3";
-    this.tools= "4" ;
-
-    this.course= { 
-        name_course: "5" ,
-        in_course: "6" ,
-        link_course: "7"
-    }
-    this.getin =gits
-    
+function view(id, value) {
+  var app = document.getElementById(id)
+  if (app.style.display == value) {
+    app.style.display = "none";
+  } else {
+    app.style.display = value;
+  }
 }
-function gits(){ 
+function app() {
+  this.name = "1";
+  this.an = "2";
+  this.adding = "3";
+  this.tools = "4";
+
+  this.course = {
+    name_course: "5",
+    in_course: "6",
+    link_course: "7"
+  }
+  this.getin = gits
+
+}
+function gits() {
   console.log(
-   this.name+"-"+
-         this.an+"-"+
-         this.adding+"-"+
-         this.tools+"-"+
-         this.course
+    this.name + "-" +
+    this.an + "-" +
+    this.adding + "-" +
+    this.tools + "-" +
+    this.course
   )
 }
